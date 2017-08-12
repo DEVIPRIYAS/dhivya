f7d3609fc4b1c1b6d2ce9da83da63e48f3999ecb
DEVIPRIYAS <30364226+DEVIPRIYAS@users.noreply.github.com>
2017-08-12 17:00:37 +0530

    Create 46.c

---
+++ 46.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main()
+{
+    int j, n;
+    printf("Enter number to print table: ");
+    scanf("%d", &n);
+    for(j=1; j<=10; j++)
+    {
+        printf("%d * %d = %d\n", n, i, (n*j));
+    }
+
+    return 0;
+}
