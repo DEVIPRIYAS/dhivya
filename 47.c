2f5249521924efbc115fdcb07a0a0b3d9eb96c14
DEVIPRIYAS <30364226+DEVIPRIYAS@users.noreply.github.com>
2017-08-23 16:13:04 +0530

    Create 47.c

---
+++ 47.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+ 
+int main()
+{
+int a[50],i,n,high,low;
+printf("How many elements:");
+scanf("%d",&n);
+printf("Enter the Array:");
+for(i=0;i<n;++i)
+scanf("%d",&a[i]);
+high=low=a[0];
+for(i=1;i<n;++i)
+{
+if(a[i]>high)
+high=a[i];
+if(a[i]<low)
+low=a[i];
+}
+printf("The highest element is %d",high);
+printf("\nThe lowest element is %d",low);
+return 0;
+}
