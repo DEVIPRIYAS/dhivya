b3f21928b274c28326ac45c18278f8d1c3afd44c
DEVIPRIYAS <30364226+DEVIPRIYAS@users.noreply.github.com>
2017-08-23 16:24:40 +0530

    Create 49.c

---
+++ 49.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+ 
+int main()
+{
+int x=0,y=0,s=0;    	
+printf("Enter two numbers...");
+scanf("%d%d",&x,&y);   
+s=x+y;                   
+printf("The sum is %d",s);
+return 0;
+}
